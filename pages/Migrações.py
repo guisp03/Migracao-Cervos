5b89ccb9d0854f14bce3895d4e8c9ea71a430c6c
Guilherme Panza <guisousapanza@gmail.com>
2024-11-04 10:13:50 -0300

    Routes visualization

--- pages/Migrações.py
+++ pages/Migrações.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pandas as pd
+import pydeck as pdk
 import streamlit as st
 
 dir_path = Path(__file__).parent.parent
@@ -13,10 +14,53 @@ def migrations_page():
         initial_sidebar_state="expanded"
     )
 
+    # pre-processing
+    deer_data["timestamp"] = pd.to_datetime(deer_data["timestamp"]) 
+
     st.title("Migração dos veados 🏃‍♀️")
     selected_deer = st.selectbox(
         "Selecione o veado",
         pd.unique(deer_data["individual-local-identifier"])
+    )
+
+    selected_deer_data = deer_data.loc[
+        deer_data["individual-local-identifier"] == selected_deer
+    ]
+
+    selected_year = st.selectbox(
+        "Selecione o ano",
+        pd.unique(selected_deer_data["timestamp"].dt.year)
+    )
+
+    filtered_deer_data = selected_deer_data.loc[
+        selected_deer_data["timestamp"].dt.year == selected_year
+    ]
+
+    path_data = filtered_deer_data[
+        ["location-long", "location-lat"]
+    ].values.tolist()
+    initial_lat = filtered_deer_data["location-lat"].median()
+    initial_long = filtered_deer_data["location-long"].median()
+
+    st.pydeck_chart(
+        pdk.Deck(
+            layers=pdk.Layer(       
+                type="PathLayer",
+                data=[{"path": path_data}],
+                pickable=True,
+                width_scale=20,
+                width_min_pixels=2,
+                get_color=[237, 28, 36],
+                get_path="path",
+                get_width=5,
+            ),
+            initial_view_state={
+                "latitude": initial_lat,
+                "longitude": initial_long,
+                "zoom": 10,
+                "pitch": 50,
+            }
+        )
     )
 
 if __name__ == "__main__":
